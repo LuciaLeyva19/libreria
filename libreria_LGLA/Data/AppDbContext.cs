363b695f80e284f00854780832fb9624846e12f6
LuciaLeyva19 <luciagabrielaleyvaalvarez@gmail.com>
2024-11-04 11:24:48 -0700

    libreria avance

---
+++ libreria_LGLA/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using libreria_LGLA.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace libreria_LGLA.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
